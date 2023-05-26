77a6402fae1cefac0db440c825f662e6a6e9b717
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-05-25 22:31:35 -0300

    feat: Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Header from "./components/Header";
 import "./global.css";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+import logo from "../assets/logo.svg";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Logo" />
+    </header>
+  );
+}
+
+export default Header;
